1f3d14162eb5f6590d083b8df97506a4ca943742
koooge <koooooge@gmail.com>
2018-03-26 01:53:10 +0900

    create ListActivity

--- feature/src/main/java/com/koooge/example/android/androidsamplemapapp/feature/MainActivity.java
+++ feature/src/main/java/com/koooge/example/android/androidsamplemapapp/feature/MainActivity.java
@@ -1,8 +1,8 @@
 package com.koooge.example.android.androidsamplemapapp.feature;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -32,8 +32,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplication(), ListActivity.class);
+                startActivity(intent);
             }
         });
     }
